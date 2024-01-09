538b18925993d54097f6972a72e6477415fd053a
Roshni <roshnigwadhwani@gmail.com>
2024-01-09 17:35:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("blockchain");
+
+  const fetchNews = () => {
+    fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.hits);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, [searchQuery]);
+
+  const handleChange = (e) => setSearchQuery(e.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>News App</h3>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange}></input>
+        <button>Search</button>
+      </form>
+      {news.map((n, index) => (
+        <p key={index}>{n.title}</p>
+      ))}
+    </>
   );
 }
 
